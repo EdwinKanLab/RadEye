cb33d48d2ad946875aa1ee7e6177bdab15a1e070
ta275 <38818430+ta275@users.noreply.github.com>
2020-06-04 11:07:23 -0400

    pc_commands ud

--- Pi/TCP/Initial/PC_Commands.cpp
+++ Pi/TCP/Initial/PC_Commands.cpp
@@ -12,11 +12,41 @@
 #include "TicDriver.h"
 // #include <ImageProcessor.hpp>
 #include <string>
+#include <math.h>
+#include <unistd.h>
+#include <vector>
 
 using namespace std;
 
 
 // ImageProcessor IP;
+const char vertical_sno [9] = "00295780";
+uint8_t step_mode = 3;
+uint8_t mult = (uint8_t) pow(2, (double)step_mode);
+uint32_t max_speed = 2000000 * mult;
+uint32_t starting_speed = 0;
+uint32_t max_decel = 40000 * mult;
+uint32_t max_accel = 40000 * mult; 
+uint16_t current_limit = 1000;
+int32_t curr_pos = 0;
+int32_t full_vertical = 83;
+TicDriver* vertical = createTicDriver(vertical_sno, max_speed, starting_speed,
+max_decel, max_accel, step_mode, current_limit, curr_pos);
+
+const vector<string> explode(const string& s, const char& c)
+{
+	string buff{""};
+	vector<string> v;
+	
+	for(auto n:s)
+	{
+		if(n != c) buff+=n; else
+		if(n == c && buff != "") { v.push_back(buff); buff = ""; }
+	}
+	if(buff != "") v.push_back(buff);
+	
+	return v;
+}
 
 
 string getCoordinate(){
@@ -30,5 +60,23 @@ string gotoCenter(){
 }
 
 string step(string command){
+    vector<string> v{explode(command, ' ')};
+    int steps = stoi(v[1]);
+
+    if (v[2].compare("LR1") == 0){
+        ;
+    }
+    else if (v[2].compare("LR2") == 0){
+        ;
+    }
+    else if (v[2].compare("UD1") == 0){
+        vertical->steps(vertical, steps);
+    }
+    else if (v[2].compare("UD2") == 0){
+        ;
+    }
+
+    
     return "04done";
 }
+
