d6e7cba1e7619cf0555ab017d89effd6eabb9886
ta275 <38818430+ta275@users.noreply.github.com>
2020-06-08 18:50:40 -0400

    update raspicam

--- Pi/Vision/C++/raspicam/main.cpp
+++ Pi/Vision/C++/raspicam/main.cpp
@@ -7,8 +7,9 @@ int main ( int argc,char **argv ) {
    
     time_t timer_begin,timer_end;
     raspicam::RaspiCam_Cv Camera;
-    cv::Mat image;
+    
     int nCount=100;
+    cv::Mat image[nCount];
     //set camera params
     Camera.set( CAP_PROP_FORMAT, CV_8UC1 );
     //Open camera
@@ -19,7 +20,7 @@ int main ( int argc,char **argv ) {
     time ( &timer_begin );
     for ( int i=0; i<nCount; i++ ) {
         Camera.grab();
-        Camera.retrieve ( image);
+        Camera.retrieve ( image[i]);
         if ( i%5==0 )  cout<<"\r captured "<<i<<" images"<<std::flush;
     }
     cout<<"Stop camera..."<<endl;
@@ -28,7 +29,9 @@ int main ( int argc,char **argv ) {
     time ( &timer_end ); /* get current time; same as: timer = time(NULL)  */
     double secondsElapsed = difftime ( timer_end,timer_begin );
     cout<< secondsElapsed<<" seconds for "<< nCount<<"  frames : FPS = "<<  ( float ) ( ( float ) ( nCount ) /secondsElapsed ) <<endl;
-    //save image 
-    cv::imwrite("raspicam_cv_image.jpg",image);
-    cout<<"Image saved at raspicam_cv_image.jpg"<<endl;
+    //save image
+    for int (i = 0; i < nCount; i++){
+        cv::imwrite("images/raspicam_cv_image_" + to_string(i) ".jpg",image);
+    }
+    cout<<"All images saved at raspicam_cv_image.jpg"<<endl;
 }
