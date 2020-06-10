eafab996d30695092ae6dca227bedad80b5bf6b4
ta275 <38818430+ta275@users.noreply.github.com>
2020-06-10 16:00:42 -0400

    tcp + image processing + raspicam integrate

--- Pi/TCP/Initial/TCP/PC_Commands.cpp
+++ Pi/TCP/Initial/TCP/PC_Commands.cpp
@@ -66,8 +66,47 @@ using namespace std;
 // ///////////////////////////////////////////////////////////////////////////
 
 // BlobDetector blobDetector;
+///////////////////////////////////////////////////////////////////////////
+float screenWidthMM;
+float screenHeightMM;
+
+///////////////////////////////////////////////////////////////////////////
+// Cropper initialDiscard(0.18, 0.98, 0.12, 0.97);
+// Cropper topLeftRegion(0, 0.10, 0.0, 0.10);
+// Cropper topRightRegion(0.05, 0.15, 0.85, 1);
+// Cropper bottomLeftRegion(0.85, 0.98, 0, 0.15);
+// Cropper bottomRightRegion(0.85, 1, 0.85, 0.99);
+
+Cropper initialDiscard(0.18, 0.98, 0.03, 0.88);
+Cropper topLeftRegion(0.05, 0.15, 0.0, 0.15);
+Cropper topRightRegion(0.0, 0.10, 0.9, 1);
+Cropper bottomLeftRegion(0.85, 1, 0.01, 0.15);
+Cropper bottomRightRegion(0.85, 0.98, 0.85, 1);
+
+///////////////////////////////////////////////////////////////////////////
+
+CornerDetector singleCorner(1,0.1,100);
+CornerDetector fourPlaneCorners(4, 0.1, 100);
+
+///////////////////////////////////////////////////////////////////////////
+
+Warper warper;
+
+///////////////////////////////////////////////////////////////////////////
+
+BlobDetector blobDetector;
+
+//////////////////////////////////////////////////////////////////////////
+GroundTruthEvaluator groundTruthEvaluator;
+
+//////////////////////////////////////////////////////////////////////////
+raspicam::RaspiCam_Cv Camera;
+
+//////////////////////////////////////////////////////////////////////////
 
 void setup(){
+    screenWidthMM = 267;
+    screenHeightMM = 180.5;
     blobDetector.setThresholdParams(200, 255, 10);
     blobDetector.setColorParams(true, 255);
     blobDetector.setAreaParams(false, 0, 2);
--- Pi/TCP/Initial/TCP/PC_Commands.hpp
+++ Pi/TCP/Initial/TCP/PC_Commands.hpp
@@ -10,64 +10,25 @@
 
 #pragma once
 #include <string>
-#include "BlobDetector/include/BlobDetector.hpp"
+// #include "BlobDetector/include/BlobDetector.hpp"
 
-#include "Cropper/include/Cropper.hpp"
+// #include "Cropper/include/Cropper.hpp"
 
-#include "CornerDetector/include/CornerDetector.hpp"
+// #include "CornerDetector/include/CornerDetector.hpp"
 
-#include "Warper/include/Warper.hpp"
+// #include "Warper/include/Warper.hpp"
 
-#include "GroundTruthEvaluator/include/GroundTruthEvaluator.hpp"
+// #include "GroundTruthEvaluator/include/GroundTruthEvaluator.hpp"
 
-#include <opencv2/core/mat.hpp>
-#include <opencv2/imgcodecs.hpp>
-#include <raspicam/raspicam_cv.h>
+// #include <opencv2/core/mat.hpp>
+// #include <opencv2/imgcodecs.hpp>
+// #include <raspicam/raspicam_cv.h>
 
 
 
 
 using namespace std;
 using namespace cv;
-
-///////////////////////////////////////////////////////////////////////////
-float screenWidthMM = 267;
-float screenHeightMM = 180.5;
-
-///////////////////////////////////////////////////////////////////////////
-// Cropper initialDiscard(0.18, 0.98, 0.12, 0.97);
-// Cropper topLeftRegion(0, 0.10, 0.0, 0.10);
-// Cropper topRightRegion(0.05, 0.15, 0.85, 1);
-// Cropper bottomLeftRegion(0.85, 0.98, 0, 0.15);
-// Cropper bottomRightRegion(0.85, 1, 0.85, 0.99);
-
-Cropper initialDiscard(0.18, 0.98, 0.03, 0.88);
-Cropper topLeftRegion(0.05, 0.15, 0.0, 0.15);
-Cropper topRightRegion(0.0, 0.10, 0.9, 1);
-Cropper bottomLeftRegion(0.85, 1, 0.01, 0.15);
-Cropper bottomRightRegion(0.85, 0.98, 0.85, 1);
-
-///////////////////////////////////////////////////////////////////////////
-
-CornerDetector singleCorner(1,0.1,100);
-CornerDetector fourPlaneCorners(4, 0.1, 100);
-
-///////////////////////////////////////////////////////////////////////////
-
-Warper warper;
-
-///////////////////////////////////////////////////////////////////////////
-
-BlobDetector blobDetector;
-
-//////////////////////////////////////////////////////////////////////////
-GroundTruthEvaluator groundTruthEvaluator;
-
-//////////////////////////////////////////////////////////////////////////
-raspicam::RaspiCam_Cv Camera;
-
-//////////////////////////////////////////////////////////////////////////
-
 
 void setup();
 
