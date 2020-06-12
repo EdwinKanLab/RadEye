cc38411e68b7cb285c0e08cfcb1c9f178a61dcd8
ta275 <38818430+ta275@users.noreply.github.com>
2020-06-12 16:40:05 -0400

    pc push

--- Pi/TCP/Initial/TCP/GroundTruthEvaluator/test/GroundTruthEvaluatorTest.cpp
+++ Pi/TCP/Initial/TCP/GroundTruthEvaluator/test/GroundTruthEvaluatorTest.cpp
@@ -12,61 +12,78 @@
 #include <opencv2/imgcodecs.hpp>
 
 #include <iostream>
-// #include <filesystem>
+
 #include <string>
-#include <chrono> 
 #include <utility>
 
 using namespace cv;
 using namespace std;
-// using path = std::filesystem::path;
 
+Point2f getCorner(Cropper& region, CornerDetector& singleCorner){
+
+    singleCorner.setInputImage(region.getResult());
+    singleCorner.doTheJob();
+    Point2f result = singleCorner.getResult()[0];
+    Point2f origin;
+    origin.x =  (float) (int) (region.getLeftRatio() * (region.getInputImage().cols));
+    origin.y = (float) (int) (region.getTopRatio() * (region.getInputImage().rows));
+    result += origin;
+
+    
+    return result;
+
+}
 
 int main(){
-
-    Mat image1 = imread("../TestImages/testImage1.jpg", IMREAD_COLOR);
-    Mat image2 = imread("../TestImages/testImage2.jpg", IMREAD_COLOR);
-    Mat image3 = imread("../TestImages/testImage3.jpg", IMREAD_COLOR);
-    Mat image4 = imread("../TestImages/testImage4.jpg", IMREAD_COLOR);
-    Mat image5 = imread("../TestImages/testImage5.jpg", IMREAD_COLOR);
-    if(!(image1.data && image2.data && image3.data && image4.data && image5.data))                              // Check for invalid input
-    {
-        cout <<  "Could not open or find one or more images" << std::endl ;
-        return -1;
-    }
-
-    Mat images[5] = {image1, image2, image3, image4, image5};
-    auto start = chrono::high_resolution_clock::now();
-    auto stop = chrono::high_resolution_clock::now();;
-    auto duration = chrono::duration_cast<chrono::microseconds>(stop - start);
-    auto total = duration.count();
-
+    
+    Mat image;
+    image = imread("../100images/raspicam_cv_image_"+
+        to_string(0)+".jpg", IMREAD_COLOR);
 
     ///////////////////////////////////////////////////////////////////////////
     float screenWidthMM = 267;
     float screenHeightMM = 180.5;
 
     ///////////////////////////////////////////////////////////////////////////
-    // Cropper initialDiscard(0.18, 0.98, 0.12, 0.97);
-    // Cropper topLeftRegion(0, 0.10, 0.0, 0.10);
-    // Cropper topRightRegion(0.05, 0.15, 0.85, 1);
-    // Cropper bottomLeftRegion(0.85, 0.98, 0, 0.15);
-    // Cropper bottomRightRegion(0.85, 1, 0.85, 0.99);
-    
+
     Cropper initialDiscard(0.18, 0.98, 0.03, 0.88);
     Cropper topLeftRegion(0.05, 0.15, 0.0, 0.15);
     Cropper topRightRegion(0.0, 0.10, 0.9, 1);
     Cropper bottomLeftRegion(0.85, 1, 0.01, 0.15);
     Cropper bottomRightRegion(0.85, 0.98, 0.85, 1);
 
+    Cropper initialDiscardB(0.18, 0.98, 0.12, 0.97);
+    Cropper topLeftRegionB(0, 0.10, 0.0, 0.10);
+    Cropper topRightRegionB(0.05, 0.15, 0.85, 1);
+    Cropper bottomLeftRegionB(0.85, 0.98, 0, 0.15);
+    Cropper bottomRightRegionB(0.85, 1, 0.85, 0.99);
     ///////////////////////////////////////////////////////////////////////////
     
     CornerDetector singleCorner(1,0.1,100);
-    // CornerDetector fourPlaneCorners(4, 0.1, 100);
     
     ///////////////////////////////////////////////////////////////////////////
 
     Warper warper;
+
+    initialDiscardB.setInputImage(image);
+    initialDiscardB.doTheJob();
+
+    topLeftRegionB.setInputImage(initialDiscardB.getResult());
+    topRightRegionB.setInputImage(initialDiscardB.getResult());
+    bottomRightRegionB.setInputImage(initialDiscardB.getResult());
+    bottomLeftRegionB.setInputImage(initialDiscardB.getResult());
+    
+    topLeftRegionB.doTheJob();
+    topRightRegionB.doTheJob();
+    bottomRightRegionB.doTheJob();
+    bottomLeftRegionB.doTheJob();
+    
+    warper.setTopLeftSrcPoint(getCorner(topLeftRegionB, singleCorner));
+    warper.setTopRightSrcPoint(getCorner(topRightRegionB, singleCorner));
+    warper.setBottomRightSrcPoint(getCorner(bottomRightRegionB, singleCorner));
+    warper.setBottomLeftSrcPoint(getCorner(bottomLeftRegionB, singleCorner));
+    warper.setInputImage(initialDiscardB.getResult());
+    warper.doTheJob();
 
     ///////////////////////////////////////////////////////////////////////////
 
@@ -81,7 +98,12 @@ int main(){
     blobDetector.setMinDistBetweenBlobs(0);
 
     ///////////////////////////////////////////////////////////////////////////
+    blobDetector.setInputImage(warper.getResult());
+    blobDetector.doTheJob();
+    cout << "Found "<<blobDetector.getResult().size() << " blobs." << endl;
+    blobDetector.saveResultAsImage("../Results/blobDetector.jpg");
 
+    ///////////////////////////////////////////////////////////////////////////
     GroundTruthEvaluator groundTruthEvaluator;
     groundTruthEvaluator.setScreenDimMM(screenWidthMM, screenHeightMM);
 
@@ -95,71 +117,16 @@ int main(){
     groundTruthEvaluator.setBlobDetector(blobDetector);
 
     ///////////////////////////////////////////////////////////////////////////
-    for (int i = 0; i < 5; i++){
-        groundTruthEvaluator.setInputImage(images[i]);
-        groundTruthEvaluator.doTheJob();
-        
-        groundTruthEvaluator.saveInputImage("../Results/"+
-        to_string(i+1)+"a_inputImage.jpg");
-        
-        groundTruthEvaluator.saveResultAsImage("../Results/"+
-        to_string(i+1)+"b_groundTruthImage.jpg");
-    }
-
-    ///////////////////////////////////////////////////////////////////////////
-    images[0].release();
-    images[1].release();
-    images[2].release();
-    images[3].release();
-    images[4].release();
-    image1.release();
-    image2.release();
-    image3.release();
-    image4.release();
-    image5.release();
 
 
-    ///////////////////////////////////////////////////////////////////////////
-    Mat image;
-    image = imread("../100images/raspicam_cv_image_"+
-        to_string(0)+".jpg", IMREAD_COLOR);
-    // start = chrono::high_resolution_clock::now();
-    cout << "Started timing" << endl;
     groundTruthEvaluator.setInputImage(image);
-    groundTruthEvaluator.saveInputImage("../100imagesResults/raspicam_cv_image_"+ 
-    to_string(0)+"_inputImage.jpg");
-    cout << "Set input image" << endl;
     groundTruthEvaluator.doTheJob();
-    cout << "Evaluated Ground Truth" << endl;
-    // cout << "Read image 0" << endl;
-
-
-    // for (int i = 0; i < 5; i++){
-    //     cout << "Reading image # " << i << endl;
-    //     image = imread("../100images/raspicam_cv_image_"+
-    //     to_string(i)+".jpg", IMREAD_COLOR);
-
-    //     if(!(image.data)){
-    //         cout <<  "Could not open or find one or more images" << endl ;
-    //         return -1;
-    //     }
-    //     cout << "Finished reading" << endl;
-
-    //     start = chrono::high_resolution_clock::now();
-    //     cout << "Started timing" << endl;
-    //     groundTruthEvaluator.setInputImage(image);
-    //     cout << "Set input image" << endl;
-    //     groundTruthEvaluator.doTheJob();
-    //     cout << "Evaluated Ground Truth" << endl;
-
-    //     stop = chrono::high_resolution_clock::now();
-        
-    //     duration = chrono::duration_cast<chrono::microseconds>(stop - start);
-    //     total += duration.count();
-    // }
-
-    // cout << "Total time for 100 images: " << total << endl;
-    cout << "Finished Test" << endl;
-    cout << "Check Results directory for the image files" << endl;
-
+    
+    groundTruthEvaluator.saveInputImage("../Results/"+
+    to_string(0)+"a_inputImage.jpg");
+    
+    groundTruthEvaluator.saveResultAsImage("../Results/"+
+    to_string(0)+"b_groundTruthImage.jpg");
+    
+    ///////////////////////////////////////////////////////////////////////////
 }
