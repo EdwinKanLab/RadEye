810327c13f63caa18a606723f8334fa0fb2dd0fb
ta275 <38818430+ta275@users.noreply.github.com>
2020-06-12 13:41:45 -0400

    pi data

---
+++ Pi/TCP/Initial/TCP/tcp_build/CMakeFiles/tcp_pi.dir/DependInfo.cmake
@@ -0,0 +1,35 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/pi/Code/Github/RadEye/Pi/TCP/Initial/TCP/BlobDetector/src/BlobDetector.cpp" "/home/pi/Code/Github/RadEye/Pi/TCP/Initial/TCP/tcp_build/CMakeFiles/tcp_pi.dir/BlobDetector/src/BlobDetector.cpp.o"
+  "/home/pi/Code/Github/RadEye/Pi/TCP/Initial/TCP/CornerDetector/src/CornerDetector.cpp" "/home/pi/Code/Github/RadEye/Pi/TCP/Initial/TCP/tcp_build/CMakeFiles/tcp_pi.dir/CornerDetector/src/CornerDetector.cpp.o"
+  "/home/pi/Code/Github/RadEye/Pi/TCP/Initial/TCP/Cropper/src/Cropper.cpp" "/home/pi/Code/Github/RadEye/Pi/TCP/Initial/TCP/tcp_build/CMakeFiles/tcp_pi.dir/Cropper/src/Cropper.cpp.o"
+  "/home/pi/Code/Github/RadEye/Pi/TCP/Initial/TCP/GroundTruthEvaluator/src/GroundTruthEvaluator.cpp" "/home/pi/Code/Github/RadEye/Pi/TCP/Initial/TCP/tcp_build/CMakeFiles/tcp_pi.dir/GroundTruthEvaluator/src/GroundTruthEvaluator.cpp.o"
+  "/home/pi/Code/Github/RadEye/Pi/TCP/Initial/TCP/PC_Commands.cpp" "/home/pi/Code/Github/RadEye/Pi/TCP/Initial/TCP/tcp_build/CMakeFiles/tcp_pi.dir/PC_Commands.cpp.o"
+  "/home/pi/Code/Github/RadEye/Pi/TCP/Initial/TCP/Tic/TicDriver.c" "/home/pi/Code/Github/RadEye/Pi/TCP/Initial/TCP/tcp_build/CMakeFiles/tcp_pi.dir/Tic/TicDriver.c.o"
+  "/home/pi/Code/Github/RadEye/Pi/TCP/Initial/TCP/Warper/src/Warper.cpp" "/home/pi/Code/Github/RadEye/Pi/TCP/Initial/TCP/tcp_build/CMakeFiles/tcp_pi.dir/Warper/src/Warper.cpp.o"
+  "/home/pi/Code/Github/RadEye/Pi/TCP/Initial/TCP/client_ii.cpp" "/home/pi/Code/Github/RadEye/Pi/TCP/Initial/TCP/tcp_build/CMakeFiles/tcp_pi.dir/client_ii.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/usr/local/include"
+  "../GroundTruthEvaluator/include"
+  "../Cropper/include"
+  "../CornerDetector/include"
+  "../Warper/include"
+  "../BlobDetector/include"
+  "../Tic"
+  "/opt/opencv-4.1.0/include/opencv4"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ Pi/TCP/Initial/TCP/tcp_build/CMakeFiles/tcp_pi.dir/cmake_clean.cmake
@@ -0,0 +1,17 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/tcp_pi.dir/client_ii.cpp.o"
+  "CMakeFiles/tcp_pi.dir/BlobDetector/src/BlobDetector.cpp.o"
+  "CMakeFiles/tcp_pi.dir/CornerDetector/src/CornerDetector.cpp.o"
+  "CMakeFiles/tcp_pi.dir/Cropper/src/Cropper.cpp.o"
+  "CMakeFiles/tcp_pi.dir/GroundTruthEvaluator/src/GroundTruthEvaluator.cpp.o"
+  "CMakeFiles/tcp_pi.dir/PC_Commands.cpp.o"
+  "CMakeFiles/tcp_pi.dir/Tic/TicDriver.c.o"
+  "CMakeFiles/tcp_pi.dir/Warper/src/Warper.cpp.o"
+  "tcp_pi.pdb"
+  "tcp_pi"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/tcp_pi.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
