5087c6362c5baf902357e7d2db49811d43ac9e5b
ta275 <38818430+ta275@users.noreply.github.com>
2020-06-15 14:11:39 -0400

    gte test update

--- Pi/TCP/Initial/TCP/GroundTruthEvaluator/test/GroundTruthEvaluatorTest.cpp
+++ Pi/TCP/Initial/TCP/GroundTruthEvaluator/test/GroundTruthEvaluatorTest.cpp
@@ -217,7 +217,11 @@ int main(){
     groundTruthEvaluator.setBlobDetector(blobDetector);
 
     // ///////////////////////////////////////////////////////////////////////////
-
+    groundTruthEvaluator.blobDetector.setInputImage(warper.getResult());
+    groundTruthEvaluator.blobDetector.doTheJob();
+    cout << "Found "<< groundTruthEvaluator.blobDetector.getResult().size() << " blobs." << endl;
+    groundTruthEvaluator.blobDetector.saveInputImage("../GTEblobInput2.jpg");
+    groundTruthEvaluator.blobDetector.saveResultAsImage("../Results/GTEblobDetector2.jpg");
 
     groundTruthEvaluator.setInputImage(image);
     groundTruthEvaluator.doTheJob();
