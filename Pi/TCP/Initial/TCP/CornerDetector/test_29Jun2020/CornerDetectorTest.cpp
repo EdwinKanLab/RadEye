80fc28395ca7ac50482e6d4110057272b2575e33
ta275 <38818430+ta275@users.noreply.github.com>
2020-06-29 14:55:48 -0400

    pi cd

--- Pi/TCP/Initial/TCP/CornerDetector/test_29Jun2020/CornerDetectorTest.cpp
+++ Pi/TCP/Initial/TCP/CornerDetector/test_29Jun2020/CornerDetectorTest.cpp
@@ -31,7 +31,7 @@ int main(){
 
     CornerDetector singleCorner(1,0.1,100);
 
-    CornerDetector fourPlaneCorners(4, 0.1, 500);
+    CornerDetector fourPlaneCorners(4, 0.1, 600);
     ///////////////////////////////////////////////////////////////////////////
 
     singleCorner.setInputImage(image1);
