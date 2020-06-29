5324207b3fb0d7d0fbc9f2fdb0647c45a038ad31
ta275 <38818430+ta275@users.noreply.github.com>
2020-06-29 17:21:39 -0400

    gte saved images

---
+++ Pi/TCP/Initial/TCP/GroundTruthEvaluator/test_29Jun2020/test_build/CMakeFiles/GroundTruthEvaluatorTest.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/GroundTruthEvaluatorTest.dir/GroundTruthEvaluatorTest.cpp.o"
+  "CMakeFiles/GroundTruthEvaluatorTest.dir/home/pi/Code/Github/RadEye/Pi/TCP/Initial/TCP/BlobDetector/src/BlobDetector.cpp.o"
+  "CMakeFiles/GroundTruthEvaluatorTest.dir/home/pi/Code/Github/RadEye/Pi/TCP/Initial/TCP/CornerDetector/src/CornerDetector.cpp.o"
+  "CMakeFiles/GroundTruthEvaluatorTest.dir/home/pi/Code/Github/RadEye/Pi/TCP/Initial/TCP/Cropper/src/Cropper.cpp.o"
+  "CMakeFiles/GroundTruthEvaluatorTest.dir/home/pi/Code/Github/RadEye/Pi/TCP/Initial/TCP/Warper/src/Warper.cpp.o"
+  "CMakeFiles/GroundTruthEvaluatorTest.dir/home/pi/Code/Github/RadEye/Pi/TCP/Initial/TCP/GroundTruthEvaluator/src/GroundTruthEvaluator.cpp.o"
+  "GroundTruthEvaluatorTest.pdb"
+  "GroundTruthEvaluatorTest"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/GroundTruthEvaluatorTest.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
